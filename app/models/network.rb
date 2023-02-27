b8844a7d166e86075b34aa3ccae96391c08cbc5c
Abel Muriithi <abelmuriithi04@gmail.com>
2023-02-27 14:41:14 +0300

    completed assignment

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,9 @@
 class Network < ActiveRecord::Base
   
+      def sorry
+        "We're sorry about passing on John Mulaney's pilot"
+      end
+      def shows
+        Show.where(network_id: self.id)
+      end
 end
